7c9a2059dd76eba919c9805e3f0392711163345a
Zayd Haque <zahaque@live.com>
2020-05-07 17:32:22 -0400

    added listener to combobox

--- TopBar.java
+++ TopBar.java
@@ -1,21 +1,31 @@
 import javax.swing.*;
 
 public class TopBar extends JPanel{
-	
+
 	JButton search;
 	JComboBox<?> adviceCB;
 	String stratList[] = {"One", "Alt", "Random", "Terrible"};
 	JTextArea searchAreaTA;
 	JLabel selStratLabel,searchAreaLabel;
-	
+
 	public TopBar() {
-		
+
 		search = new JButton("ADD");
 		searchAreaTA = new JTextArea(1,6);
 		selStratLabel = new JLabel("Select Investment Strategy");
 		searchAreaLabel = new JLabel("ADD Stock");
 		adviceCB = new JComboBox<Object>(stratList);
-		
+		adviceCB.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JComboBox<String> combo = (JComboBox<String>) e.getSource();
+		        String selectedStrat = (String) combo.getSelectedItem();
+		        // TODO: CHANGE THIS SO it saves selectedStrat to something that can be accessed
+		        System.out.println(selectedStrat);
+			}
+		});
+
 		add(searchAreaLabel);
 		add(searchAreaTA);
 		add(search);
