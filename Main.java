d1ee4d0133917c79a0849e84c4716f73f40115aa
Zayd Haque <zahaque@live.com>
2020-05-08 11:43:31 -0400

    added code to skip brackets in JSON

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+
 /*TODO:
 	1.READ STOCK DATA IN TO POPULATE ARRAYLIST TO ADD TO TABLE
 	2.OBSERVER PATTERN (UPDATE USER PRO, ADVICE)
@@ -16,14 +17,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		ArrayList <Stock> stockList = new ArrayList<Stock>();
 		Frame frame = new Frame();
 		Stock temp = new Stock();
 
 		URL url = null;
 		try {
-			url = new URL("https://financialmodelingprep.com/api/v3/search?query=&limit=100&exchange=NASDAQ");
+			url = new URL("https://financialmodelingprep.com//api/v3/search?query=AA&limit=10&exchange=NYSE");
 		} catch (MalformedURLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -31,22 +31,17 @@ public class Main {
 
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
 		    for (String line; (line = reader.readLine()) != null;) {
+					if(line.equals("[ {") || (line.equals("} ]"))  || (line.equals("}, {"))){
+							continue;
+					}
+					else{
+						System.out.println(line);
 
-		    if(line.equals("[ {")) {
-		    	continue;
-		    }
-
-		    else if(line.equals("} ]")) {
-		    	continue;
-		    }
-
-		    else{
-			    System.out.println(line);
-
+					}
 		    }
 
 		  }
-		} catch (UnsupportedEncodingException e) {
+		 catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
